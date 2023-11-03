0ef2873bb3517a32295e0806319c1bf82d2d8962
Slava Poliakov <78slavapolyakov@gmail.com>
2023-11-03 21:32:30 +0200

    Fixed build

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
 
       rollupOptions: {
+        external: 'simplelightbox',
         input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
